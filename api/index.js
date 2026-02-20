ff95395d16aafc46da75e053a2769b25f729e37f
Carlos Nascimento <80932010+carlospph@users.noreply.github.com>
2026-02-20 13:14:03 -0300

    Fix typo in comment for buscarTodosUsuarios import

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import db from './db.js';
-import { buscarTodosUsuarios } from './models/UsuarioModel.js'; //pegar os daods no banco
+import { buscarTodosUsuarios } from './models/UsuarioModel.js'; //pegar os dados no banco
 
 const app = express();
 app.use(express.json());
